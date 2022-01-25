b1da0908d261f760fcf01477e09ad74520d032f4
NadiaH <nadiyahrb79@gmail.com>
2022-01-25 08:38:35 +0100

    consts

---
+++ src/api/consts.ts
@@ -0,0 +1,26 @@
+export const BASE_URL = 'https://pokeapi.co/api/v2';
+export const POKEMONS = 'pokemon';
+export const POKEMONS_PER_PAGE = 12;
+export const TYPES = 'type';
+export const COLOR_TYPES: Colors = {
+  normal: '#a8a878',
+  fighting: '#c03028',
+  flying: '#a890f0',
+  poison: '#a040a0',
+  ground: '#e0c068',
+  rock: '#b8a038',
+  bug: '#a8b820',
+  ghost: '#705898',
+  steel: '#b8b8d0',
+  fire: '#f08030',
+  water: '#6890f0',
+  grass: '#78c850',
+  electric: '#f8d030',
+  psychic: '#f85888',
+  ice: '#98d8d8',
+  dragon: '#7038f8',
+  dark: '#705848',
+  fairy: '#ee99ac',
+  unknown: '#68a090',
+  shadow: '#d4eff0',
+};
